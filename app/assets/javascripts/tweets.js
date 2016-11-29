626202a88267253ecb5743746f064890514ea922
Tyler <tcameronsmith@gmail.com>
2016-11-29 18:30:07 -0500

    add ajax callback when tweet, function prepend tweets to top of ul, change redirect to render html partial

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+
+	$('.create_tweet').on('submit', function(eventObject){
+		eventObject.preventDefault();
+		console.log('part 1');
+		$.ajax({
+			url: $(this).attr('action'),
+			method: $(this).attr('method'), //why when set to 'post' doesn't this work??
+			data: $(this).serialize(),
+			dataType: 'html'
+		}).done(function(responseData) {
+			console.log(responseData);
+			$('.tweets').prepend(responseData);
+		});
+
+	});
+});
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,25 +1,30 @@
 class TweetsController < ApplicationController
-  def index
-    @tweets = Tweet.all.order(created_at: :desc)
-    @tweet = Tweet.new
-  end
+	def index
+		@tweets = Tweet.all.order(created_at: :desc)
+		@tweet = Tweet.new
+	end
 
-  def create
-    @tweet = Tweet.new(tweet_params)
+	def create
+		@tweet = Tweet.new(tweet_params)
 
-    if @tweet.save
-      redirect_to tweets_path
-    else
-      render :index
-    end
-  end
+		if @tweet.save
 
-  def destroy
-  end
+			respond_to do |format|
+					format.html { render partial: 'partial' }
 
-  private
+					format.json { render json: @tweet }
+				end
+		else
+			render :index
+		end
+	end
 
-  def tweet_params
-    params.require(:tweet).permit(:message)
-  end
+	def destroy
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:message)
+	end
 end
